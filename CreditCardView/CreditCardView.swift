1dcb16a9fa949a817c1ac3435ec5ddb2d3ca8841
Berkekacar <kacarberke@icloud.com>
2024-08-30 01:13:09 +0300

    credit card view

--- CreditCardView/CreditCardView.swift
+++ CreditCardView/CreditCardView.swift
@@ -7,18 +7,37 @@
 
 import SwiftUI
 
-struct CreditCardView: View {
+struct UserCreditCardPageView: View {
+    @State private var creditCardInfo: CreditCardInfo = CreditCardInfo()
+    @State private var flip: Bool = false
+    @State private var degrees: Double = 0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                if !flip {
+                    CreditCardFrontSubView(creditCardInfo: creditCardInfo)
+                } else {
+                    CredictCardBackView(creditCardInfo: creditCardInfo)
+                }
+            }
+            .rotation3DEffect(
+                .degrees(degrees),
+                axis: (x: 0.0, y: 1.0, z: 0.0)
+            )
+            CreditCardFormView(creditCardInfo: $creditCardInfo){
+                withAnimation {
+                    degrees += 180
+                    flip.toggle()
+                }
+            }
+            Spacer()
         }
-        .padding()
-    }
-}
+  
+        .ignoresSafeArea(.keyboard)
+        .navigationTitle("Credit Card")
+        .navigationBarBackButtonHidden(true)
 
-#Preview {
-    CreditCardView()
+    }
+
 }
